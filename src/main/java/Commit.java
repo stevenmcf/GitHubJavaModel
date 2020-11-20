7471fada24deb9ad352191dac1c98860b0af7cae
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-20 15:39:28 +0000

    initial commit - class & enum structure set-up.

---
+++ src/main/java/Commit.java
@@ -0,0 +1,12 @@
+public class Commit {
+
+    private int id;
+    private String description;
+
+    public Commit(int id, String description){
+        this.id = id;
+        this.description = description;
+    }
+
+
+}
---
+++ src/test/java/CommitTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommitTest {
+
+    Commit commit
+
+    @Before
+    public void setUp(){
+        commit = new Commit(1, "Initial commit");
+    }
+
+    @Test
+    
+}
