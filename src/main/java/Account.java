182d2c87334dfafdbb83715919985ba28b58e438
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-21 10:43:58 +0000

    basic account TDD now complete also

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -4,15 +4,37 @@ public class Account {
 
 //    instance variables
     private String userName;
-    private String name;
+    private String accName;
     private AccountType accountType;
     private ArrayList<Repo>repos;
 
 //constructor function
-    public Account (String userName, String name, AccountType accountType){
+    public Account (String userName, String accName, AccountType accountType){
         this.userName = userName;
-        this.name = name;
+        this.accName = accName;
         this.accountType = accountType;
         this.repos = new ArrayList<Repo>();
     }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public String getAccountName(){
+        return this.accName;
+    }
+
+    public AccountType getAccountType(){
+        return this.accountType;
+    }
+
+    public void addRepo(Repo repo){
+        this.repos.add(repo);
+    }
+
+    public int countRepos(){
+        return this.repos.size();
+    }
+
+
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,17 +1,67 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
 
     Account account;
     Account account1;
     Account account2;
+    Repo repo;
+    Repo repo1;
+    Repo repo2;
 
     @Before
     public void setUp(){
-        account = new Account("PJ10", "Eddie Vedder", AccountType.PRO);
-        account1 = new Account("JL95", "Jonah Lomu", AccountType.FREE);
-        account2 = new Account("ImASmashingPumpkin", "Billy Corgan", AccountType.PRO);
-
+        account = new Account("Eddie Vedder", "PJ10", AccountType.PRO);
+        account1 = new Account("Jonah Lomu", "JL95", AccountType.FREE);
+        account2 = new Account("Billy Corgan", "ImASmashingPumpkin", AccountType.PRO);
+        repo = new Repo("Pearl Jam", "My band on tour", RepoType.PRIVATE );
+        repo1 = new Repo("World Cup '95 Highlight", "I ran over tony underwood", RepoType.PUBLIC);
+        repo2 = new Repo("Mellon Collie", "Story of a sad dog", RepoType.PRIVATE);
     }
+    @Test
+    public void hasUserName(){
+        assertEquals("Eddie Vedder", account.getUserName());
+    }
+
+//    @Test
+//    public void hasUserName(){
+//        assertEquals("Jonah Lomu", account1.getUserName());
+//    }
+//
+//    @Test - why does this test fail when trying to run more than once but assertEquals is for the different set-ups above ?
+//    public void hasUserName(){
+//        assertEquals("Billy Corgan", account2.getUserName());
+//    }
+
+    @Test
+    public void hasAccountName(){
+        assertEquals("PJ10", account.getAccountName());
+    }
+
+//    @Test
+//    public void hasAccountName(){
+//        assertEquals("JL95", account1.getAccountName());
+//    }
+//
+//    @Test
+//    public void hasAccountName(){
+//        assertEquals("Story of a sad dog", account2.getAccountName());
+//    }
+
+    @Test
+    public void hasAccountType(){
+        assertEquals(AccountType.PRO, account.getAccountType());
+    }
+
+
+    @Test
+    public void canAddRepository(){
+        account2.addRepo(repo2);
+        assertEquals(1, account2.countRepos() );
+    }
+
 
 }
